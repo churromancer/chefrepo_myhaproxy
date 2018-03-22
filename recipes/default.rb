d31961fb8d7a8e66e65041a1ea49ede757858a4d
churromancer <churromancer@gmail.com>
2018-03-22 13:11:20 -0600

    Uploaded changes made during lab exercises to include two load balancers

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,23 @@
+# chef-repo/cookbooks/myhaproxy/recipes/default.rb
 #
-# Cookbook:: myhaproxy
+# Cookbook Name:: myhaproxy
 # Recipe:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+node.default['haproxy']['members'] = [{
+    'hostname' => 'ip-172-31-13-207.us-west-1.compute.internal', # these values come from the Vagrantfile
+    'ipaddress' => '172.31.13.207', # or the public IP and hostname is using a cloud provider
+    'port' => 80,
+    'ssl_port' => 80
+  },
+  
+  {
+    'hostname' => 'ip-172-31-11-151.us-west-1.compute.internal', # these values come from the Vagrantfile
+    'ipaddress' => '172.31.11.151', # or the public IP and hostname is using a cloud provider
+    'port' => 80,
+    'ssl_port' => 80
+  }
+]
+
+include_recipe 'haproxy::manual'
